50f555e47801197e2eb5d2ee2420291a9b6dd364
JoyyCLangat <121197381+JoyyCLangat@users.noreply.github.com>
2025-06-19 23:27:23 +0300

    Quicksort

---
+++ Arrays/Quicksort3.py
@@ -0,0 +1,13 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[0]
+
+    smaller = [x for x in arr if x < pivot]
+    equal = [x for x in arr if x == pivot]
+    larger = [x for x in arr if x > pivot]
+
+    return quick_sort(smaller) + equal + quick_sort(larger)
+
+print(quick_sort([6, 3, 8, 1, 5]))
---
+++ Arrays/Quicksort4.py
@@ -0,0 +1,14 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    
+    pivot = arr[-1]
+
+    smaller = [x for x in arr if x < pivot]
+    equal = [x for x in arr if x == pivot]
+    larger = [x for x in arr if x > pivot]
+
+    return quick_sort(smaller) + equal + quick_sort(larger)
+
+arr1 = quick_sort([4, 2, 7, 2, 1, 8, 4])
+print(arr1[-1])
