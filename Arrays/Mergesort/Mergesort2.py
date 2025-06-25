a5246a5a7a03c507826d1760d6c730cee368e714
JoyyCLangat <121197381+JoyyCLangat@users.noreply.github.com>
2025-06-25 22:14:58 +0300

    Mergesort

--- Arrays/Mergesort/Mergesort2.py
+++ Arrays/Mergesort/Mergesort2.py
@@ -1,6 +1,37 @@
-def merge_sort(arr):
-    if len(arr) <=1 :
-        return arr
+class ListNode:
+    def __init__(self,val=0, next=None):
+        self.val = val
+        self.next = next
+
+def sortList(head):
+    if not head or not head.next:
+        return head
     
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
+    slow, fast = head, head.next
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    mid = slow.next
+    slow.next = None
+
+    left = sortList(head)
+    right = sortList(mid)
+
+    return merge(left, right)
+
+def merge(l1, l2):
+    dummy = ListNode(0)
+    tail = dummy
+
+    while l1 and l2:
+        if l1.val < l2.val:
+            tail.next = l1
+            l1 = l1.next
+        else:
+            tail.next = l2
+            l2 = l2.next
+        tail = tail.next
+
+    tail.next = l1 or l2
+    return dummy.next
