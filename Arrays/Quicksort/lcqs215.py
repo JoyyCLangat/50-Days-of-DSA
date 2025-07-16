24427c716266028fe73ad8334200da1305aaaeef
JoyyCLangat <121197381+JoyyCLangat@users.noreply.github.com>
2025-07-16 21:59:23 +0300

    lcqs

---
+++ Arrays/Quicksort/lcqs215.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        import heapq
+        
+        # Min-Heap approach: O(n log k) time, O(k) space
+        heap = []
+        
+        for num in nums:
+            if len(heap) < k:
+                heapq.heappush(heap, num)
+            elif num > heap[0]:
+                heapq.heapreplace(heap, num)
+        
+        return heap[0]
